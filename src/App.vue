01541ea68c493e25d6c2af6ab818e0eca9d606b3
sherry-huang <58199296+sherry-huang@users.noreply.github.com>
2021-04-12 15:17:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="app">
+        <div>
+            <NewCarousel />
+        </div>
+        <!-- <div class="table-box">
+            <NewTable />
+            <NewTable />
+        </div> -->
+    </div>
+</template>
+
+<script>
+import NewCarousel from "./components/NewCarousel.vue";
+// import NewTable from "./components/NewTable.vue"
+
+export default {
+    name: "App",
+    methods: {
+        say: function (message) {
+            alert(message)
+        }
+    },
+    components: {
+        NewCarousel,
+        // NewTable
+    },
+    data() {
+        return {
+            title: "030",
+            vHtml: "<p>ˊ ˇ`/</p>",
+            user: {
+                name: "vuuue",
+            },
+            items: [{ text: "Hello" }, { text: "Love You" }],
+            condition: true,
+        };
+    },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#app .table-box {
+    margin: 50px 0 0 0;
+}
+</style>
---
+++ src/components/NewCarousel.vue
@@ -0,0 +1,76 @@
+<template>
+  <a-carousel dots dots-class="slick-dots slick-thumb">
+    <a slot="customPaging" slot-scope="props">
+      <img :src="getImgUrl(props.i)" />
+    </a>
+    <div v-for="item in urlArr" :key="item">
+      <img :src="item" />
+    </div>
+  </a-carousel>
+</template>
+<script>
+const baseUrl1 = 'https://pic.pimg.tw/acatandcats/1344952155-375602234.jpg';
+const baseUrl2 = 'https://images2.gamme.com.tw/news2/2017/49/24/q6CVnZ2YlKSdqw.jpg';
+const baseUrl3 = 'https://4.bp.blogspot.com/-FI6LfVLyjWk/Xpq4MCak6MI/AAAAAAAADuU/_Cn5CV7xE9QUj5zT9EJcYTB4i1Ot4sdVQCLcBGAsYHQ/s1600/5cbc5e7c10abfdbb2c0edb6112de3234_s.jpg';
+const baseUrl =
+  'https://raw.githubusercontent.com/vueComponent/ant-design-vue/master/components/vc-slick/assets/img/react-slick/';
+
+export default {
+  data() {
+    return {
+      baseUrl,
+      baseUrl1,
+      baseUrl2,
+      baseUrl3,
+      urlArr: [baseUrl1, baseUrl2, baseUrl3]
+    };
+  },
+  methods: {
+    getImgUrl(i) {
+      switch (i) {
+        case 0:
+          return baseUrl1
+        case 1:
+          return baseUrl2
+        default:
+          return baseUrl3
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+/* For demo */
+.ant-carousel {
+  text-align: center;
+  height: 300px;
+  line-height: 160px;
+  background: #364d79;
+  /* overflow: hidden; */
+}
+.ant-carousel >>> .slick-dots {
+  height: auto;
+}
+.ant-carousel >>> .slick-slide img {
+  /* border: 5px solid #fff; */
+  display: block;
+  margin: auto;
+  max-width: 80%;
+  height: 300px;
+}
+.ant-carousel >>> .slick-thumb {
+  bottom: -45px;
+}
+.ant-carousel >>> .slick-thumb li {
+  width: 60px;
+  height: 45px;
+}
+.ant-carousel >>> .slick-thumb li img {
+  width: 100%;
+  height: 100%;
+  filter: grayscale(100%);
+}
+.ant-carousel >>> .slick-thumb li.slick-active img {
+  filter: grayscale(0%);
+}
+</style>
